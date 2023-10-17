e27b1a1a0b5d4b6a89dcdc7ad45afe90b7911717
rota.20196 <rota.20196@E2-9-PC11>
2023-10-17 12:49:05 +0200

    <accellera, frena>

--- Rota Thomas - 7 - Classe Automobile/Automobile.cs
+++ Rota Thomas - 7 - Classe Automobile/Automobile.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Rota_Thomas___7___Classe_Automobile
 {
-     class Automobile
+    class Automobile
     {
+        protected bool _status;
+        protected int _vel;
+        protected int _marcia;
+
+        public Automobile()
+        {
+            _status = false;
+            _vel = 0;
+            _marcia = 0;
+        }
+
+        public bool status { get { return _status; } set { _status = value; } }
+        public int velocità { get { return _vel; } set { _vel = value; } }
+        public int marcia { get { return _marcia; } set { _marcia = value; } }
+
+        public int AccendiAuto()
+        {
+            if (status == false)
+            {
+                status = true;
+                return 1;
+            }
+            return 0;
+        }
+        public int SpegniAuto()
+        {
+            if (status == true)
+            {
+                status = false;
+                return 1;
+            }
+            return 0;
+        }
+        public int Accellera()
+        {
+            if (status == true && marcia > 0)
+            {
+                velocità += 30;
+                return 1;
+            }
+            return 0;
+        }
     }
 }
