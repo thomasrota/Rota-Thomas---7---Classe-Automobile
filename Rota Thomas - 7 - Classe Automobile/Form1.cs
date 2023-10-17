1899d54b0e6aa1a5c2442ac15d8196b4c2ffbe2b
rota.20196 <rota.20196@E2-9-PC11>
2023-10-17 12:33:29 +0200

    Aggiungere i file di progetto.

---
+++ Rota Thomas - 7 - Classe Automobile/Form1.cs
@@ -0,0 +1,10 @@
+namespace Rota_Thomas___7___Classe_Automobile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
